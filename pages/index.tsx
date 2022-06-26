728f8cd6af9bc2fa8f0b254355b1eb42f07b8b1b
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2022-06-26 19:31:46 +0100

    contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,7 @@ import starShape from "../public/assets/images/star-shape.svg"
 import yellowBlackFlower from "../public/assets/images/yellow-black-flower.svg"
 import pointingArrow from "../public/assets/arrows/pointing-arrow.svg"
 import Review from '../components/review'
+import Contact from '../components/sections/Contact'
 
 
 const Home: NextPage = () => {
@@ -210,25 +211,7 @@ const Home: NextPage = () => {
       {/* End of Appointment */}
 
       {/* Contact */}
-      <section className="layout-container mt-20 text-textColor max-w-[790px] pb-[247px] md:pb-[519px]">
-        <div className="text-center">
-          <h2 className={`mob-h2 md:heading-2 md:mb-[21px] ${styles.jeko_regular}`}>Contact</h2>
-          <h3 className="font-inter body-text px-5 md:heading-3 md:mb-[54px] md:px-10">
-            We always love to hear from our customers. So feel free to get in
-            touch today to learn more about what we have to offer.
-          </h3>
-          <div className={`caption font-inter mt-8 space-y-2 w-3/4 mx-auto xs:w-3/5 md:heading-3 md:${styles.jeko_regular} `}>
-            <div className="flex justify-between">
-              <p>Phone Number:</p>
-              <p>{footer_contact.text[1]}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Email Address:</p>
-              <p>{footer_contact.text[0]}</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Contact />
       {/* End of Contact */}
 
     </Layout>
